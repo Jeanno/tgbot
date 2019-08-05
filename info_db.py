7121450d73aae098da0f8b14edb4d1a9964c87b8
Jeanno <jeanno.cheung@gmail.com>
2019-08-05 16:38:12 +0800

    Timeout set to 10 seconds

--- info_db.py
+++ info_db.py
@@ -14,7 +14,7 @@ def fetch():
         # 'majorDimension': 'COLUMNS',
         'key': key
     }
-    r = session.get(url, params=params, timeout=1)
+    r = session.get(url, params=params, timeout=10)
     data = r.json()
     return data['values']
 
